77fa7aeab266cf4e17b5246bf0ac075f64d3cc68
Marlon Monzon <martianworm17@gmail.com>
2019-11-17 12:07:54 +0800

    Get the string from the user

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Input a string: ");
+
+        Scanner sc = new Scanner(System.in);
+
+        String theString = sc.nextLine();
+
+        System.out.println("The input string is: " + theString);
     }
 }
